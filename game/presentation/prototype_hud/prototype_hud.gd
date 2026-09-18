9daae82cfa74e84dcc3f3f10538a01f0e7a388ea
bageus <36338626+bageus@users.noreply.github.com>
2026-09-19 01:47:34 +0300

    Adapt HUD behavior to rebuilt reference layout

--- game/presentation/prototype_hud/prototype_hud.gd
+++ game/presentation/prototype_hud/prototype_hud.gd
@@ -7,7 +7,6 @@ extends CanvasLayer
 @onready var mutation_bar: ProgressBar = $HealthPanel/MutationBar
 @onready var mutation_value: Label = $HealthPanel/MutationValue
 @onready var critical_marker: ColorRect = $HealthPanel/CriticalMarker
-@onready var armor_segments: HBoxContainer = $HealthPanel/ArmorSegments
 @onready var antidote_count: Label = $AntidotePanel/AntidoteCount
 @onready var weapon_name: Label = $WeaponPanel/WeaponName
 @onready var magazine: Label = $WeaponPanel/Magazine
@@ -28,6 +27,11 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
+	_update_vitals()
+	_update_weapon()
+
+
+func _update_vitals() -> void:
 	hp_bar.value = player.health
 	hp_value.text = "%d / %d" % [roundi(player.health), roundi(player.max_health)]
 
@@ -35,24 +39,26 @@ func _process(_delta: float) -> void:
 	var critical: float = infection.call("get_critical_threshold")
 	mutation_bar.value = mutation
 	mutation_value.text = "%d / 100" % roundi(mutation)
-	critical_marker.position.x = 80.0 + 260.0 * clampf(critical / 100.0, 0.0, 1.0)
-
-	var filled := ceili(player.armor / maxf(player.max_armor, 1.0) * armor_segments.get_child_count())
-	for i in armor_segments.get_child_count():
-		var segment := armor_segments.get_child(i) as ColorRect
-		segment.color = Color(0.08, 0.82, 1.0, 1.0) if i < filled else Color(0.025, 0.12, 0.18, 0.9)
-
+	critical_marker.position.x = 78.0 + 264.0 * clampf(critical / 100.0, 0.0, 1.0)
 	antidote_count.text = str(player.antidotes)
+
+
+func _update_weapon() -> void:
 	var weapon: Node = player.get_current_weapon()
-	weapon_name.text = weapon.call("get_weapon_name")
+	weapon_name.text = weapon.call("get_weapon_name").to_upper()
 	var magazine_ammo: int = weapon.call("get_magazine_ammo")
 	var reserve_ammo: int = weapon.call("get_reserve_ammo")
 	var reloading: bool = weapon.call("is_reloading")
-	var empty: bool = magazine_ammo == 0
+	var empty := magazine_ammo == 0
 
 	magazine.text = str(magazine_ammo)
 	reserve.text = "/ %d" % reserve_ammo
+	_update_reload_message(reloading, empty, reserve_ammo)
+	_update_weapon_warning(empty)
+	_update_weapon_slots(empty)
 
+
+func _update_reload_message(reloading: bool, empty: bool, reserve_ammo: int) -> void:
 	if reloading:
 		reload_label.text = "RELOADING"
 	elif empty and reserve_ammo > 0:
@@ -62,15 +68,20 @@ func _process(_delta: float) -> void:
 	else:
 		reload_label.text = ""
 
+
+func _update_weapon_warning(empty: bool) -> void:
 	var warning_color := Color(1.0, 0.12, 0.08, 1.0)
-	var normal_weapon_color := Color(0.82, 0.93, 1.0, 1.0)
+	var normal_color := Color(0.82, 0.93, 1.0, 1.0)
 	weapon_name.modulate = warning_color if empty else Color.WHITE
 	magazine.modulate = warning_color if empty else Color.WHITE
-	reload_label.modulate = warning_color if empty and not reloading else Color(0.1, 0.9, 1.0, 1.0)
+	reload_label.modulate = warning_color if empty else Color(0.1, 0.9, 1.0, 1.0)
 	for part in gun_parts:
-		part.color = warning_color if empty else normal_weapon_color
+		part.color = warning_color if empty else normal_color
 
+
+func _update_weapon_slots(empty: bool) -> void:
 	var active_index: int = player.get_current_weapon_index()
+	var warning_color := Color(1.0, 0.12, 0.08, 1.0)
 	for i in slot_frames.size():
 		if i == active_index:
 			slot_frames[i].modulate = warning_color if empty else Color(0.05, 1.0, 1.0, 1.0)
