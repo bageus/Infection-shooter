87cb69cd5d29ba623fa40f7765b891680c488377
bageus <36338626+bageus@users.noreply.github.com>
2026-09-22 17:20:38 +0300

    Hide planner light markers during gameplay

---
+++ game/presentation/office_floor/planner_light.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var marker: MeshInstance3D = $Marker
+@onready var light: OmniLight3D = $Light
+
+func _ready() -> void:
+	add_to_group("planner_lights")
+	set_planning_visual(false)
+
+func set_planning_visual(enabled: bool) -> void:
+	if marker != null:
+		marker.visible = enabled
+	if light != null:
+		light.visible = true
