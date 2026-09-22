d58c70201996d8ddcbed21f101030f6f106549fd
bageus <36338626+bageus@users.noreply.github.com>
2026-09-22 15:42:53 +0300

    Make chair pushing deterministic

--- game/presentation/office_floor/pushable_chair.gd
+++ game/presentation/office_floor/pushable_chair.gd
@@ -1,4 +1,4 @@
-extends RigidBody3D
+extends AnimatableBody3D
 
 @export var push_impulse := 2.4
 @export var max_horizontal_speed := 4.5
@@ -7,11 +7,7 @@ var _health := 45.0
 
 func _ready() -> void:
 	_health = max_health
-	continuous_cd = true
-	contact_monitor = true
-	max_contacts_reported = 6
 	add_to_group("pushable_chair")
-	lock_rotation = true
 
 func push_from_character(character_position: Vector3, movement: Vector3) -> void:
 	var direction := global_position - character_position
@@ -22,17 +18,12 @@ func push_from_character(character_position: Vector3, movement: Vector3) -> void
 		direction = direction.normalized()
 	else:
 		direction = Vector3.FORWARD
-	sleeping = false
-	apply_central_impulse(direction * push_impulse)
+	var distance := push_impulse * 0.06
+	var motion := direction * distance
+	move_and_collide(motion)
 
-func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	var v := state.linear_velocity
-	var horizontal := Vector2(v.x, v.z)
-	if horizontal.length() > max_horizontal_speed:
-		horizontal = horizontal.normalized() * max_horizontal_speed
-		v.x = horizontal.x
-		v.z = horizontal.y
-		state.linear_velocity = v
+func _physics_process(_delta: float) -> void:
+	pass
 
 func take_projectile_hit(damage: float, _hit_position: Vector3, _hit_normal: Vector3, direction: Vector3, weapon_name: String) -> bool:
 	var multiplier := 1.35 if weapon_name == "SHOTGUN" else 1.0
@@ -40,7 +31,7 @@ func take_projectile_hit(damage: float, _hit_position: Vector3, _hit_normal: Vec
 	var impulse_direction := direction
 	impulse_direction.y = 0.0
 	if impulse_direction.length_squared() > 0.001:
-		apply_central_impulse(impulse_direction.normalized() * 0.8)
+		move_and_collide(impulse_direction.normalized() * 0.12)
 	if _health <= 0.0:
 		queue_free()
 	return true
@@ -50,6 +41,6 @@ func take_melee_hit(damage: float, _hit_position: Vector3, direction: Vector3) -
 	var impulse_direction := direction
 	impulse_direction.y = 0.0
 	if impulse_direction.length_squared() > 0.001:
-		apply_central_impulse(impulse_direction.normalized() * 1.4)
+		move_and_collide(impulse_direction.normalized() * 0.16)
 	if _health <= 0.0:
 		queue_free()
